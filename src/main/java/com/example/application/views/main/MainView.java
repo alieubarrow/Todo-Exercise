e2873a58d8455097e228dace15b7810a9cc9b8ab
alieu <alieu@DESKTOP-9QJSO48>
2022-07-11 20:07:02 -0700

    MainView

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -4,11 +4,9 @@ import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @Route("") 
@@ -18,17 +16,9 @@ public class MainView extends VerticalLayout {
 
   public MainView() {
     VerticalLayout todosList = new VerticalLayout(); 
-
-
     TextField taskField = new TextField(); 
-
-
     Button addButton = new Button("Add"); 
-
-
     addButton.addClickListener(click -> { 
-
-
       Checkbox checkbox = new Checkbox(taskField.getValue());
       todosList.add(checkbox);
     });
